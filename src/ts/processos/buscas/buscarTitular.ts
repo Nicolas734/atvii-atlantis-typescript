58148ff6377df41f431ac27fbe2a76cfe889c87c
Nicolas Lima <nichollaslimma734@gmail.com>
2022-11-09 17:21:50 -0300

    criado arquivo de buscar cliente titular e ajustado editar titular

---
+++ src/ts/processos/buscas/buscarTitular.ts
@@ -0,0 +1,41 @@
+import Processo from "../../abstracoes/processo";
+import Armazem from "../../dominio/armazem";
+import Cliente from "../../modelos/cliente";
+import Documento from "../../modelos/documento";
+import ListagemTitulares from "../listagens/listagemTitulares";
+
+export default class BuscarTitular extends Processo{
+    private titular!: Cliente
+    private clientes!: Cliente[]
+
+    constructor(){
+        super()
+        this.clientes = Armazem.InstanciaUnica.Clientes
+    }
+
+    processar(): Cliente {
+        
+        this.processo = new ListagemTitulares()
+        this.processo.processar()
+        
+        let numeroDocumento = this.entrada.receberTexto("Digite o numero do documento do titular desejado: ")
+
+        this.clientes.forEach( (cliente: Cliente) => {
+            cliente.Documentos.forEach( (documento:Documento) => {
+                if( documento.Numero === numeroDocumento){
+                    this.titular = cliente
+                }
+            })
+        })
+
+        return this.titular
+    }
+
+    private verificarTitular(cliente: Cliente): boolean {
+        let verificacao = false
+        if (cliente.Titular == undefined) {
+            verificacao = true
+        }
+        return verificacao
+    }
+}
--- src/ts/processos/editar/editarClienteTitular.ts
+++ src/ts/processos/editar/editarClienteTitular.ts
@@ -1,33 +1,62 @@
 import Processo from "../../abstracoes/processo";
-import MenuOpcaoBuscaCliente from "../../menus/menusEditar/menuOpcaoBuscaCliente";
-import BuscarTitularPorDocumento from "../buscarTitularPorDocumento";
-import ListagemTitulares from "../listagens/listagemTitulares";
+import Cliente from "../../modelos/cliente";
+import MenuOpcaoEditar from '../../menus/menusEditar/menuOpcaoEditar';
+import EditarDadosCliente from "./editarDadosCliente";
+import EditarTelefoneCliente from "./editarTelefoneCliente";
+import EditarEnderecoCliente from "./editarEnderecoCliente";
+import EditarDocumentoCliente from "./editarDocumentoCliente";
+import BuscarTitular from "../buscas/buscarTitular";
+import Impressor from "../../interfaces/impressor";
+import ImpressorClienteDocumento from "../../impressores/impressorClienteDocumento";
 
 export default class EditarClienteTitular extends Processo{
-    processar(): void {
-        this.menu = new MenuOpcaoBuscaCliente()
+    private impressor!: Impressor
+
+    constructor(){
+        super()
+        this.menu = new MenuOpcaoEditar()
         this.execucao = true
+    }
 
-        while(this.execucao){
+    processar(): void {
 
-            this.menu.mostrar()
-            this.opcao = this.entrada.receberNumero('Qual opção desejada?')
+        this.processo = new BuscarTitular()
+        let titular = this.processo.processar();
 
-            switch(this.opcao){
-                case 1:
-                    this.processo = new ListagemTitulares()
-                    this.processo.processar()
-                    break
-                case 2:
-                    this.processo = new BuscarTitularPorDocumento()
-                    this.processo.processar()
-                    break
-                case 0:
-                    this.execucao = false
-                    break
-                default:
-                    console.log('Operação não entendida.');
+        if(titular === undefined){
+            console.log("Titular não encontrado...");
+        }else{
+            this.impressor = new ImpressorClienteDocumento(titular)
+            console.log(this.impressor.imprimir())
 
+            while(this.execucao){
+
+                this.menu.mostrar()
+                this.opcao = this.entrada.receberNumero('Qual opção desejada?')
+
+                switch(this.opcao){
+                    case 1:
+                        this.processo = new EditarDadosCliente(titular)
+                        this.processo.processar()
+                        break
+                    case 2:
+                        this.processo = new EditarTelefoneCliente(titular)
+                        this.processo.processar()
+                        break
+                    case 3:
+                        this.processo = new EditarEnderecoCliente(titular)
+                        this.processo.processar()
+                        break
+                    case 4:
+                        this.processo = new EditarDocumentoCliente(titular)
+                        this.processo.processar()
+                        break
+                    case 0:
+                        this.execucao = false
+                        break
+                    default:
+                        console.log('Operação não entendida.');
+                }
             }
         }
     }
